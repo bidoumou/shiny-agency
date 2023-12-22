f798f4ea442219f4491830ee6459ad00b501ed16
bidoumou <williamlg@free.fr>
2023-12-22 11:44:11 +0100

    Initialisation de l'exercice

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+function Card({ label, title = 'Mon titre par défaut', picture }) {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+            <span>{label}</span>
+            <img src={picture} alt="freelance" height={80} width={80} />
+            <span>{title}</span>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    label: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    picture: PropTypes.string,
+}
+
+Card.defaultProps = {
+    title: 'Mon titre par défaut',
+}
+
+export default Card
